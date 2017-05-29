e1945e927f884785daac772f53d5575e06bfdaa4
rahul-kumi <kayrahul@gmail.com>
2017-05-29 12:42:15 +0530

    archiving code from 2K16

---
+++ MultiSerial1/MultiSerial1.ino
@@ -0,0 +1,26 @@
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  sensorValue = analogRead(sensorPin);
+
+  // read from port 0, send to port 1:
+  //if (Serial.available()) {
+   // int inByte = Serial.read();
+    if(sensorValue>505)
+    Serial.write('l');
+    else
+    Serial.write('r');
+    
+    Serial.println();
+    delay(500);
+  //}
+}
